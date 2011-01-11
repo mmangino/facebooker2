75e68b5586a2ad8bc91e46b25f7a5f56eb56ba0a
Mike Mangino <mmangino@elevatedrails.com>
2011-01-11 08:55:24 -0500

    Fix bug from klochner

--- lib/facebooker2/rails/helpers/javascript.rb
+++ lib/facebooker2/rails/helpers/javascript.rb
@@ -41,7 +41,13 @@ module Facebooker2
           </script>
         JAVASCRIPT
         escaped_js = fb_html_safe(js)
-        block_given? ? concat(escaped_js) : escaped_js
+        if block_given? 
+           concat(escaped_js)
+           #return the empty string, since concat returns the buffer and we don't want double output
+           # from klochner
+           "" 
+        else
+           escaped_js
         end
       end
     end
